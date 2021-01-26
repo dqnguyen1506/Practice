31e469cb3efc8ddaa50fa3f9228a95a47abdbaa0
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-01-26 09:05:27 -0800

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,10 +1,3 @@
-'''
-Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-'''
 import java.util.ArrayList; 
 import java.util.Arrays; 
 import java.util.List;
@@ -15,7 +8,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(map.containsKey(complement) && nums[map.get(complement)] != nums[i]){
+            if(map.containsKey(complement)){
                 return new int[] {i, map.get(complement)};
             }
             map.put(nums[i], i);
@@ -25,9 +18,6 @@ class Solution {
     public static void main (String[] args){
         int[] nums = new int[]{3,2,4}; 
         Solution sol = new Solution();
-        for (int i : result){
-            System.out.println(i);
-        }
         int[] result = sol.twoSum(nums,6);
     }
 }
