e9e0a1c9619c0c74566f4b910a65a08f22b108d8
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-01-28 16:40:43 -0800

    Create Reverse Integer.java
    Reverse Integer solution Ver. 2

---
+++ Reverse Integer Ver. 2/Reverse Integer.java
@@ -0,0 +1,25 @@
+import jdk.incubator.jpackage.internal.Arguments;
+
+import java.math.*;
+class Solution {
+    public int reverse (int x){
+        String converted = String.valueOf(Math.abs(x));
+        //System.out.println(converted);
+        String rev = "";
+        for (int i = converted.length() - 1; i >= 0; i--){
+            //System.out.println(converted.charAt(i));
+            rev += converted.charAt(i);
+        }
+        try{
+            return (x < 0) ? Integer.parseInt(rev) * -1 : Integer.parseInt(rev);
+        }
+        catch(NumberFormatException e){
+            return 0;
+        }
+        
+    }
+    public static void main (String[] args){
+        Solution sol = new Solution();
+        System.out.println(sol.reverse(153423649));
+    }
+}
