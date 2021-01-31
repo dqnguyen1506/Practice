6539b3cfa5c928db933407c372fccc4804f34d4b
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-01-30 21:02:26 -0800

    Update Solution.py

--- Zigzag Conversion/Solution.py
+++ Zigzag Conversion/Solution.py
@@ -1,3 +1,4 @@
+import math
 class Solution(object):
     def convert(self, s, numRows):
         """
@@ -6,5 +7,16 @@ class Solution(object):
         :rtype: str
         """
         print(len(s))
+        list1 = []
+        inc = ((numRows - 1) + (numRows - 2))
+        i = 0
+        while (i < (len(s) - inc)):
+            print(i)
+            print(inc + i)
+            list1.append(s[i])
+            list1.append(s[inc + i])
+            i+=1
+        print(list1)
+
 sol = Solution()
 sol.convert("PAHNAPLSIIGYIR", 4)
