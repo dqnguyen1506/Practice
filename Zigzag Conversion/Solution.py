670d7a43c0d103e1fb94ef12ded7b9d2ba63fb35
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-01-30 20:46:43 -0800

    Update Solution.py

---
+++ Zigzag Conversion/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        print(len(s))
+sol = Solution()
+sol.convert("PAHNAPLSIIGYIR", 4)
