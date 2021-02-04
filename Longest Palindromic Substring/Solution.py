fa96bdd0968be336e2dfde6c570b967479528342
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-02-03 20:17:36 -0800

    Update Solution.py

--- Longest Palindromic Substring/Solution.py
+++ Longest Palindromic Substring/Solution.py
@@ -9,25 +9,25 @@ class Solution(object):
         start = 0 
         end = 0
         for i in range (len(s)):
-            len1 = self.expandFromMiddle(s, i, i)
-            len2 = self.expandFromMiddle(s, i, i + 1)
-            length = max(len1, len2)
-            print("len1: ", len1)
-            print("len2: ", len2)
-            if (length > (end - start)):        #racecar #abaaba
+            len1 = self.expandFromMiddle(s, i, i)               #handle case 1: "racecar" ==> with 'e' in the center
+            len2 = self.expandFromMiddle(s, i, i + 1)           #handle case 2: "babbab" ==> "bb" in the center
+            length = max(len1, len2)                            #finding the greatest length
+            #print("len1: ", len1)
+            #print("len2: ", len2)
+            if (length > (end - start)):                        #if there is a longer palindrome, adjust the start and end to the new palindrome
                 start = i - ((length - 1)//2)
                 end = i + (length //2)
-        print(start, end)
+        #print(start, end)
         return s[start:end + 1]
     def expandFromMiddle(self, s, left, right):
-        if(len(s) == 0 or left > right):
+        if(len(s) == 0 or left > right):                        
             return 0
-        while(left >= 0 and right < len(s) and s[left] == s[right]):
+        while(left >= 0 and right < len(s) and s[left] == s[right]):    #keep expanding left and right while they are the same (a palindrome)
             left -= 1
             right += 1
             #print(left)
             #print(right)
-        return right - left - 1
+        return right - left - 1                                 # "- 1" b/c when 'while' loop ends, left = -1/ right = len(s) ==> not the correct indices
 sol = Solution()
 answer = sol.longestPalindrome("racecar")
 print(answer)
