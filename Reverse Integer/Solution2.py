036fafa149ea8362e75b4e496122c7ded247a85d
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-02-05 13:20:00 -0800

    Create Solution2.py

---
+++ Reverse Integer/Solution2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if(x < 0): k = -1
+        else: k = 1
+        x = int(str(abs(x))[::-1])
+        if(x <= -2**31 or x >= 2**31 - 1):
+            return 0
+        return x * k
